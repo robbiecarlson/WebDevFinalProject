c1dc4a695d7dc57884a0a588fa9bc38cf9d845cd
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-12-15 22:01:57 -0700

    Add files via upload

--- app/second-page.js
+++ app/second-page.js
@@ -1,28 +1,17 @@
-"use client";
-
-import { useState } from "react";
-//import { useRouter } from 'next/router';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import ItemList from './item-list';
 import NewItem from './new-item';
 import itemsData from './items.json';
 
-export default function Page() {
-    const [items, setItems] = useState(itemsData);
-    //const router = useRouter();
-
-    const handleAddItem = (newItem) => {
-        const newItems = [...items, newItem];
-        setItems(newItems);
-        //router.push('/second-page');
-    };
+export default function SecondPage() {
+    const [items, setItems] = useState([]);
+    const router = useRouter();
 
     return (
         <main className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
         <div>
-            <div>
-                <h1 className="text-4xl ml-10 mt-4 font-bold justify-center">Screen Score</h1>
-            </div>
+           
             <div className="mt-4 flex item-center justify-center w-full">
                 <div className="justify-center">
                     <NewItem onAddItem={handleAddItem} />
